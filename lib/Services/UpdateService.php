cbc668b5154af9549c300f7f22960bfaa34f8f00
Alex Standiford <alex@standiford.us>
2024-08-06 08:47:09 -0400

    Support for transaction filtering

---
+++ lib/Services/UpdateService.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace PHPNomad\Update\Services;
+
+use PHPNomad\Events\Interfaces\EventStrategy;
+use PHPNomad\Update\Events\UpgradeRoutinesRequested;
+use PHPNomad\Update\Interfaces\UpgradeRoutine;
+use PHPNomad\Utils\Helpers\Arr;
+
+class UpdateService
+{
+    protected EventStrategy $events;
+
+    public function __construct(EventStrategy $events)
+    {
+        $this->events = $events;
+    }
+
+    /**
+     * Gets the list of routines, sorted in the order they should run.
+     *
+     * @param string $currentVersion
+     * @param string $targetVersion
+     * @return UpgradeRoutine[]
+     */
+    protected function getRoutines(string $currentVersion, string $targetVersion)
+    {
+        $event = new UpgradeRoutinesRequested($currentVersion, $targetVersion);
+        $this->events->broadcast($event);
+        $routines = $event->getRoutines();
+
+        Arr::sort($routines, fn($a, $b) => version_compare($a::getTargetVersion(), $b::getTargetVersion()));
+
+        return $routines;
+    }
+
+    /**
+     * Runs the update based on the provided set of versions.
+     *
+     * @param string $currentVersion
+     * @param string $targetVersion
+     * @return void
+     */
+    public function update(string $currentVersion, string $targetVersion)
+    {
+        $routines = $this->getRoutines($currentVersion, $targetVersion);
+
+        Arr::each($routines, fn(UpgradeRoutine $routine) => $routine->up());
+    }
+}
